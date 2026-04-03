fd73f407e257e7e248ef27f05576eeb27439b54b
Ikram Ud Daula <ikramhasib007@gmail.com>
2026-04-04 01:53:04 +0600

    Updates

--- src/events/coupon-deleted-event.ts
+++ src/events/coupon-deleted-event.ts
@@ -1,9 +1,10 @@
 import { Streams } from './streams'
 import { Subjects } from './subjects'
+import { CouponType } from './types/coupon-type'
 
 export interface CouponDeletedEvent {
   subjectRoot: Subjects.CouponSrv
   subject: Subjects.CouponDeleted
   streamName: Streams.CouponStream
-  data: { id: string; version: number }
+  data: { id: string; version: number; type: CouponType }
 }
