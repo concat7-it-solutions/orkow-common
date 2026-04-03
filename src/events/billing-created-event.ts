f8294751a33c2c0cf3074a7c20d370422ba58511
Ikram Ud Daula <ikramhasib007@gmail.com>
2026-04-03 21:34:15 +0600

    Updates

--- src/events/billing-created-event.ts
+++ src/events/billing-created-event.ts
@@ -1,10 +1,14 @@
 import { Streams } from './streams'
 import { Subjects } from './subjects'
-import { CreateBillingInput } from './types/billing-create-input'
 
 export interface BillingCreatedEvent {
   subjectRoot: Subjects.EnrollsSrv
   subject: Subjects.BillingCreated
   streamName: Streams.EnrollsStream
-  data: CreateBillingInput & { id: string; amount: number }
+  data: {
+    id: string
+    amount: number
+    currency: string
+    billingAddressId?: string
+  }
 }
