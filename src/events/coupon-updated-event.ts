fdc52d370ab946b901ee1a36f7421d5e76015c45
Ikram Ud Daula <ikramhasib007@gmail.com>
2026-04-04 01:41:58 +0600

    Updates

--- src/events/coupon-updated-event.ts
+++ src/events/coupon-updated-event.ts
@@ -3,25 +3,18 @@ import { Subjects } from './subjects'
 import { CouponType } from './types/coupon-type'
 import { DiscountType } from './types/discount-type'
 
-export type CouponUpdatedEventData = {
-  id: string
-  version: number
-  coupon: string
-  type: CouponType
-  discountType: DiscountType
-  discountValue: number
-  maxDiscountValue?: number
-}
-
-export type CouponActionEventData = {
-  id: string
-  version: number
-  isActive: boolean
-}
-
 export interface CouponUpdatedEvent {
   subjectRoot: Subjects.CouponSrv
   subject: Subjects.CouponUpdated
   streamName: Streams.CouponStream
-  data: CouponUpdatedEventData | CouponActionEventData
+  data: {
+    id: string
+    version: number
+    coupon: string
+    type: CouponType
+    discountType: DiscountType
+    discountValue: number
+    maxDiscountValue?: number
+    isActive?: boolean
+  }
 }
