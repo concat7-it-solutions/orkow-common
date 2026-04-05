4e08783f0aca762b041ca55d04ccedbab4cd8439
Ikram Ud Daula <ikramhasib007@gmail.com>
2026-04-05 20:20:51 +0600

    Updates

--- src/events/types/user-type.ts
+++ src/events/types/user-type.ts
@@ -1,9 +1,11 @@
 export enum UserType {
   Admin = 'ADMIN',
+  AcquisitionExecutive = 'ACQUISITION_EXECUTIVE',
   Agent = 'AGENT',
   Business = 'BUSINESS',
   CustomerSupport = 'CUSTOMER_SUPPORT',
   Editor = 'EDITOR',
+  Insight = 'INSIGHT',
   Moderator = 'MODERATOR',
   User = 'USER',
   Viewer = 'VIEWER',
